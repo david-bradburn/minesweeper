c049b92a0629dd4c84b4f2a9b8c8ac1df9fa0f3e
Daniel Larby <dan_larby@hotmail.co.uk>
2019-09-11 21:56:43 +0100

    updated display function

--- board_displays.py
+++ board_displays.py
@@ -2,22 +2,37 @@ import colorama
 
 
 class TerminalDisplay:
+    color_dict = {
+        1: colorama.Fore.BLUE,
+        2: colorama.Fore.GREEN,
+        3: colorama.Fore.RED,
+        4: colorama.Fore.BLACK,
+        5: colorama.Fore.MAGENTA,
+        6: colorama.Fore.LIGHTBLUE_EX,
+        7: colorama.Fore.BLACK,
+        8: colorama.Fore.LIGHTYELLOW_EX,
+    }
+
     def __init__(self):
         pass
 
     def display(self, board):
-        for row in board:
+        print("  " + "".join(["{:3}".format(i) for i in range(len(board[0, :]))]))
+        for r, row in enumerate(board):
+            print(f"{r:2} ", end="")
             for val in row:
                 colors = ""
                 if val == 0:
+                    colors += colorama.Back.WHITE
                     val = "///"
                 elif val == 101:
+                    colors += colorama.Back.LIGHTWHITE_EX + colorama.Fore.BLACK
                     val = " X "
-                    colors += colorama.Back.WHITE + colorama.Fore.BLACK
                 elif val == 100:
-                    colors += colorama.Fore.RED
-                    val = " B "
+                    colors += colorama.Back.RED + colorama.Fore.BLACK
+                    val = " X "
                 elif 1 <= val <= 8:
+                    colors += colorama.Back.LIGHTWHITE_EX + self.color_dict[val]
                     val = f" {val} "
 
                 val = str(val)
